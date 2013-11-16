b00c85ac3c3f0327483f5a3e6d0d62f9882acb56
ecdr <Github.0hv@gishpuppy.com>
2013-11-15 18:26:30 -0800

    SAM - platform_int - uart rx, init

--- src/platform/sam34/platform_int.c
+++ src/platform/sam34/platform_int.c
@@ -24,6 +24,8 @@ extern u8 platform_timer_int_periodic_flag[ NUM_TIMER ];
 extern Pio * const pio_base[];
 extern const u32 pio_id[];
 
+extern Usart* const uart_base[];
+
 // TODO: See if library files provide a name for this
 #define PLATFORM_TIMER_COUNT_MAX ( 0xFFFFFFFFUL )
 
@@ -32,6 +34,11 @@ extern const u32 pio_id[];
 
 void negedge_handler(uint32_t id, uint32_t mask);
 void posedge_handler(uint32_t id, uint32_t mask);
+
+
+// FIXME: - need to find actual functions or macros for these
+extern u8 getport(u32 id);
+extern u8 MASK_TO_PIN(u32 mask);
 
 
 
@@ -57,9 +64,12 @@ const IRQn_Type timer_irq[] = { TC0_IRQn, TC1_IRQn, TC2_IRQn, TC3_IRQn,
 
 static void uart_common_rx_handler( elua_int_resnum resnum )
 {
-//  MAP_UARTIntClear( uart_base[ resnum ], uart_int_mask );
-//  while( MAP_UARTCharsAvail( uart_base[ resnum ] ) )  
-//    cmn_int_handler( INT_UART_RX, resnum );  
+  u32 status;
+
+	status = usart_get_status(uart_base[resnum]);
+	if (status & US_CSR_RXRDY)
+    while( usart_is_rx_ready(uart_base[resnum]) )
+      cmn_int_handler( INT_UART_RX, resnum );
 }
 
 // __attribute__((__interrupt__))  - this was in avr platform ??
@@ -233,23 +243,41 @@ void TC8_Handler()
 // Helpers
 
 // Get GPIO interrupt status as a mask
+/*
 static int inth_gpio_get_int_status( elua_int_resnum resnum )
 {
 }
+*/
 
 // ****************************************************************************
 // Interrupt: INT_UART_RX
 
 static int int_uart_rx_get_status( elua_int_resnum resnum )
-{  
+{
+  return (usart_get_interrupt_mask(uart_base[ resnum ]) & US_IMR_RXRDY) ? 1 : 0;
 }
 
 static int int_uart_rx_set_status( elua_int_resnum resnum, int status )
 {
+  int prev = int_uart_rx_get_status( resnum );
+
+  if( status == PLATFORM_CPU_ENABLE )
+    usart_enable_interrupt(uart_base[ resnum ], US_IER_RXRDY);
+  else
+    usart_disable_interrupt(uart_base[ resnum ], US_IDR_RXRDY);
+  return prev;
 }
 
+
+// FIXME: Is there any way to read status without clearing it?  Maybe need to make own status cache?
 static int int_uart_rx_get_flag( elua_int_resnum resnum, int clear )
 {
+  int flag = ( usart_get_status( uart_base[ resnum ] ) & US_CSR_RXRDY ) ? 1 : 0;
+
+  UNUSED( clear );
+/*  if( clear )
+    MAP_UARTIntClear( uart_base[ resnum ], uart_int_mask ); */
+  return flag;
 }
 
 // ****************************************************************************
@@ -309,7 +337,7 @@ static int int_gpio_negedge_get_flag( elua_int_resnum resnum, int clear )
   u32 pinmask = 1 << PLATFORM_IO_GET_PIN( resnum );
 
   if( pio_get(port, PIO_TYPE_PIO_INPUT, pinmask) != 0 )
-    return 0;
+    return 0;   // Might there be cases where want interrupt on pin with alternate function?
   if( pio_get_interrupt_status(port) & pinmask )
   {
     if( clear )
@@ -325,9 +353,7 @@ static int int_gpio_negedge_get_flag( elua_int_resnum resnum, int clear )
 
 static int int_tmr_match_get_status( elua_int_resnum resnum )
 {
-  Tc * tmr = tc(resnum);
-  u32 channel = tchanel(resnum);
-//  return tc_get_interrupt_mask(tmr, channel);
+  return (tc_get_interrupt_mask(tc(resnum), tchanel(resnum)) & TC_IMR_CPCS) ? 1 : 0;
 }
 
 static int int_tmr_match_set_status( elua_int_resnum resnum, int status )
@@ -362,29 +388,48 @@ static int int_tmr_match_get_flag( elua_int_resnum resnum, int clear )
   return status && tmr_is_enabled( resnum ) ? 1 : 0;
 }
 
+// FIXME: tmr_is_enabled - not written
+
 // ****************************************************************************
 // Interrupt initialization
 
 
-#warning Interrupt init not written
+#warning Interrupt init not tested
+
+// Set priority levels for various interrupts
+//#define USART_INT_PRI 
+//#define TIMER_INT_PRI 
+//#define PIO_INT_PRI   
 
 void platform_int_init(void)
 {
-// PIO
-//  pio_enable_interrupt(PIOA, PIO_PA16);
-//  NVIC_EnableIRQ(pio_irq[port_id]);
+  unsigned id;
 
-//  tc_enable_interrupt (tc[tcid], uint32_t ul_channel, uint32_t ul_sources);
-//  NVIC_EnableIRQ(PIOA_IRQn);
+// PIO
+  for (id = 0; id < NUM_PIO; id++) {
+#ifdef   PIO_INT_PRI
+    NVIC_SetPriority(pio_irq[id], PIO_INT_PRI);
+#endif
+    NVIC_EnableIRQ(pio_irq[id]);
+  }
+
+// Timer
+  for (id = 0; id < NUM_TIMER; id++) {
+#ifdef   TIMER_INT_PRI
+    NVIC_SetPriority(timer_irq[id], TIMER_INT_PRI);
+#endif
+    NVIC_EnableIRQ(timer_irq[id]);
+  }
     
 // USART
-//	NVIC_SetPriority(USART_INT_IRQn, USART_INT_LEVEL);
-//	NVIC_EnableIRQ(USART_INT_IRQn);
-//	usart_enable_rx(USART_BASE);
-	// Enable interrupts
-  //  usart_enable_interrupt(uart_base[uartid], US_IER_RXRDY);  // Int when receive character
-//  NVIC_EnableIRQ(USART_SERIAL_IRQ); // Need to figure out IRQ from uartid
+  for (id = 0; id < NUM_UART; id++) {
+#ifdef   USART_INT_PRI
+  NVIC_SetPriority(uart_irq[id], USART_INT_PRI);
+#endif
+    NVIC_EnableIRQ(uart_irq[id]);
+  };
 }
+
 
 // ****************************************************************************
 // Interrupt table
@@ -400,7 +445,7 @@ const elua_int_descriptor elua_int_table[ INT_ELUA_LAST ] =
 
 #else // #if defined( BUILD_C_INT_HANDLERS ) || defined( BUILD_LUA_INT_HANDLERS )
 
-// ASF provides default handlers, so don't need to provide dummies
+// ASF provides default handlers, so don't need dummies here
 
 
 #endif // #if defined( BUILD_C_INT_HANDLERS ) || defined( BUILD_LUA_INT_HANDLERS )
